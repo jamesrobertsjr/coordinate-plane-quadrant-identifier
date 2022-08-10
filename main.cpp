7dd48f67de297edde0030bb7979412fc83d80eef
jameserv <106282058+jameserv@users.noreply.github.com>
2022-08-10 11:27:00 -0400

    Added do..while loop for program rerunning

--- main.cpp
+++ main.cpp
@@ -6,63 +6,70 @@
 #include <iomanip>
 #include <string>
 #include <cmath>
-
 using namespace std;
+
+char validateEntry (char yesNo);
 
 int main()
 {
-    int x, y;                       // Stores user-inputted data.
+    int x, y;
+    char yesNo;
 
-    // Asks the user for a coordinate.
-    cout << "Enter an integer coordinate: x y\n";
-    cin >> x >> y;
+    do
+    {
+        // Asks the user for a coordinate.
+        cout << "Enter an integer coordinate: x y\n";
+        cin >> x >> y;
 
-
-    // Origin.
-    if (x == 0 && y == 0)
-        cout << "(" << x << "," << y << ")" << " is on the origin.\n";
-
-
-    // First Quadrant.
-    if (x > 0 && y > 0)
-        cout << "(" << x << "," << y << ")" << " is in the first quadrant.\n";
+        // Origin.
+        if (x == 0 && y == 0)
+            cout << "(" << x << "," << y << ")" << " is on the origin.\n";
 
 
-    // Second Quadrant.
-    if (x < 0 && y > 0)
-        cout << "(" << x << "," << y << ")" << " is in the second quadrant.\n";
+        // First Quadrant.
+        if (x > 0 && y > 0)
+            cout << "(" << x << "," << y << ")" << " is in the first quadrant.\n";
 
-    // Third Quadrant.
-    if (x < 0 && y < 0)
-        cout << "(" << x << "," << y << ")" << " is in the third quadrant.\n";
 
-    // Fourth Quadrant.
-    if (x > 0 && y < 0)
-        cout << "(" << x << "," << y << ")" << " is in the fourth quadrant.\n";
+        // Second Quadrant.
+        if (x < 0 && y > 0)
+            cout << "(" << x << "," << y << ")" << " is in the second quadrant.\n";
 
-    // X-Axis.
-    if (y == 0 && x != 0)
-        cout << "(" << x << "," << y << ")" << " is on the x-axis.\n";
+        // Third Quadrant.
+        if (x < 0 && y < 0)
+            cout << "(" << x << "," << y << ")" << " is in the third quadrant.\n";
 
-    // Y-Axis.
-    if (x == 0 && y != 0)
-        cout << "(" << x << "," << y << ")" << " is on the y-axis.\n";
+        // Fourth Quadrant.
+        if (x > 0 && y < 0)
+            cout << "(" << x << "," << y << ")" << " is in the fourth quadrant.\n";
 
+        // X-Axis.
+        if (y == 0 && x != 0)
+            cout << "(" << x << "," << y << ")" << " is on the x-axis.\n";
+
+        // Y-Axis.
+        if (x == 0 && y != 0)
+            cout << "(" << x << "," << y << ")" << " is on the y-axis.\n";
+        
+        // Asks to run again.
+        cout << "Would you like to run again? (Y or N) ";
+        cin >> yesNo;
+
+    } while (validateEntry(yesNo) == 'Y');
+    
     return 0;
 }
 
-/*
- Program Output Sample 1:
-
- Enter an integer coordinate: x y
- 4 6
- (4,6) is in the first quadrant.
-
- Program Output Sample 2:
-
- Enter an integer coordinate: x y
- -3 -6
- (-3,-6) is in the third quadrant.
-
- */
-
+// Validates user entry for re-running program.
+char validateEntry (char yesNo)
+{
+    // Checks if the user submitted a proper entry. If not, it asks for a valid entry.
+    while (toupper(yesNo) != 'Y' && toupper(yesNo) != 'N')
+    {
+        cout << "ERROR. Invalid entry. Please try again." << endl;
+        cin >> yesNo;
+    }
+    
+    // Returns the valid entry.
+    return toupper(yesNo);
+}
